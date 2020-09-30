73d98f8e8947bf983bde06d39121cde7771a36bd
AsheeshKumar34 <asheesh.kumar3496@gmail.com>
2020-09-30 22:10:13 +0530

    Updates

---
+++ precache-manifest.fd30e38a0d0b1a31b0449469755115f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9fd6cfeafbcb166ca1c0029a07e1df",
+    "url": "/travel-website/index.html"
+  },
+  {
+    "revision": "131fc4bdbc5a3b45bd1c",
+    "url": "/travel-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf213cf863d5a3787a6b",
+    "url": "/travel-website/static/js/2.82dd61e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travel-website/static/js/2.82dd61e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131fc4bdbc5a3b45bd1c",
+    "url": "/travel-website/static/js/main.0f56b509.chunk.js"
+  },
+  {
+    "revision": "d580d396f7b79288f196",
+    "url": "/travel-website/static/js/runtime-main.0d8913de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travel-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
